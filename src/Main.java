9307038460bca2cc54ebbeb6c5810751fa90cb55
aleksandraurchenko <alex_bloom@mail.ru>
2022-03-17 20:16:50 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+        public static void main(String[] args) {
+
+
+            int ticketPrice = 100;     //стоимость билета
+            int bonusMilePrice = 20;   //стоимость одной мили
+            int bonusMileAmount = ticketPrice / bonusMilePrice;
+
+
+            System.out.println("количество начисленных миль за купленный билет:");
+            System.out.println((ticketPrice / bonusMilePrice) + "миль");
+
+
+        }
+        }
