d90c6bd3e541260995c9ff56c7089f33dee8556c
aleksandraurchenko <alex_bloom@mail.ru>
2022-03-17 20:23:36 +0300

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
 
-            int ticketPrice = 100;     //стоимость билета
-            int bonusMilePrice = 20;   //стоимость одной мили
-            int bonusMileAmount = ticketPrice / bonusMilePrice;
+        int ticketPrice = 100;     //стоимость билета
+        int bonusMilePrice = 20;   //стоимость одной мили
+        int bonusMileAmount = ticketPrice / bonusMilePrice;
 
 
-            System.out.println("количество начисленных миль за купленный билет:");
-            System.out.println((ticketPrice / bonusMilePrice) + "миль");
+        System.out.println("количество начисленных миль за купленный билет:");
+        System.out.println((bonusMileAmount) + "миль");
 
 
-        }
-        }
+    }
+}
